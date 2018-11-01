7e3ac1caad0ff1fc7791cfe72596b1edc88b084e
Steven Coutts <stevec@couttsnet.com>
2018-11-01 10:08:20 +0000

    Change to Forfusion

--- tutorial.py
+++ tutorial.py
@@ -34,17 +34,17 @@ This is the minimal configuration necessary to enable packet forwarding
 within the ACI fabric.
 """
 # Create the Tenant
-tenant = Tenant('softcat-test')
+tenant = Tenant('forfusion-test')
 
 # Create the Application Profile
-app = AppProfile('sc-app', tenant)
+app = AppProfile('for-app', tenant)
 
 # Create the EPG
-epg = EPG('sc-epg', app)
+epg = EPG('for-epg', app)
 
 # Create a Context and BridgeDomain
-context = Context('sc-default-vrf', tenant)
-bd = BridgeDomain('sc-default-bd', tenant)
+context = Context('for-default-vrf', tenant)
+bd = BridgeDomain('for-default-bd', tenant)
 bd.add_context(context)
 
 # Place the EPG in the BD
